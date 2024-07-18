7a65d708f10dc4961ae12d5d800582ffb5cdac50
Vinícius Marques <viniciusboz1000@outlook.com>
2024-07-18 11:33:57 -0300

    Nomes das variáveis dos switches trocados para identificação

--- ReedSwitch/ReedSwitch.ino
+++ ReedSwitch/ReedSwitch.ino
@@ -1,24 +1,24 @@
-const int reedSwitchPin1 = 32; 
-const int reedSwitchPin2 = 26;
-int reedState1 = 0;            // Variável para armazenar o estado do reed switch
-int reedState2 = 0;            // Variável para armazenar o estado do reed switch
+const int reedSwitchPinDoor = 32; 
+const int reedSwitchPinHandle = 26;
+int reedStateDoor = 0;            // Variável para armazenar o estado do reed switch
+int reedStateHandle = 0;            // Variável para armazenar o estado do reed switch
 
 void setup() {
-  pinMode(reedSwitchPin1, INPUT);   // pino para o primeiro reedSwitch  
-  pinMode(reedSwitchPin2, INPUT);  // pino para o segundo reedSwitch
+  pinMode(reedSwitchPinDoor, INPUT);   // pino para o primeiro reedSwitch  
+  pinMode(reedSwitchPinHandle, INPUT);  // pino para o segundo reedSwitch
   Serial.begin(9600);             // Inicializa a comunicação serial a 9600 bps
 }
 
 void loop() {
-  reedState1 = digitalRead(reedSwitchPin1);  // Lê o estado do primeiro reed switch
-  reedState2 = digitalRead(reedSwitchPin2);  // Lê o estado do segundo reed switch
-  if (reedState1 == LOW) {   // Se o reed switch está fechado (ímã próximo)
+  reedStateDoor = digitalRead(reedSwitchPinDoor);  // Lê o estado do primeiro reed switch
+  reedStateHandle = digitalRead(reedSwitchPinHandle);  // Lê o estado do segundo reed switch
+  if (reedStateDoor == LOW) {   // Se o reed switch está fechado (ímã próximo)
     Serial.println("Reed switch 1 fechado");
   } else {
     Serial.println("Reed switch 1 aberto");
   }
 
-  if (reedState2 == LOW) {   // Se o reed switch está fechado (ímã próximo)
+  if (reedStateHandle == LOW) {   // Se o reed switch está fechado (ímã próximo)
     Serial.println("Reed switch 2 fechado");
   } else {
     Serial.println("Reed switch 2 aberto");
